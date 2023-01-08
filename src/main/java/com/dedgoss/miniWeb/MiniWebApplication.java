8865c9db0d70669e03251bb984975866ee0c5017
Dedgoss <418420214@qq.com>
2023-01-08 21:28:59 +0800

    init springboot application

---
+++ src/main/java/com/dedgoss/miniWeb/MiniWebApplication.java
@@ -0,0 +1,13 @@
+package com.dedgoss.miniWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniWebApplication.class, args);
+	}
+
+}
